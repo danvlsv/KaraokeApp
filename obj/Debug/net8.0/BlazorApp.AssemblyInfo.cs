89fe44e6f3fad00b81fa4e15064967e7407355e6
dan_vlsv <129579067+danvlsv@users.noreply.github.com>
2024-10-26 21:59:18 +0500

    Add .gitignore

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3b6ed1ed2458c202fc87bdead96ab099447422")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
