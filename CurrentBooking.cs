ab1f831e85cd0c36d3f6885358e7f9447fa05341
dan_vlsv <129579067+danvlsv@users.noreply.github.com>
2024-11-07 00:53:17 +0500

    Major changes to multiple components and their styles, singleton improvements

--- CurrentBooking.cs
+++ CurrentBooking.cs
@@ -8,9 +8,9 @@ namespace BlazorApp
 		public int RoomNumber { get; private set; }  // Забронированная комната
 		public string Date { get; private set; } // Дата
 		public int? Time { get; private set; } // Время
-		public string Name { get; private set; } // Имя
-        public string Phone { get; private set; } // Телефон
-        public string Extra { get; private set; } = string.Empty; // Дополнительная информация
+        public string Name { get; private set; } = ""; // Имя
+        public string Phone { get; private set; } = "";// Телефон
+        public string Extra { get; private set; } // Дополнительная информация
 
         public event Action OnChange;
 
@@ -48,11 +48,29 @@ namespace BlazorApp
             RoomNumber = 0;
 			Time = null;
 			Date = null;
+            Name = null;
+            Phone = null;
+            Extra = null;
 		}
 
         private void NotifyStateChanged()
         {
             OnChange?.Invoke();
         }
-    }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+		public void SetPhone(string phone)
+		{
+			Phone = phone;
+		}
+
+		public void SetExtra(string extra)
+		{
+			Extra = extra;
+		}
+	}
 }
